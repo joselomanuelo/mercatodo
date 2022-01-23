a5b3e77225dc8b717d7a79d9b55a4242f41d9a16
jomgarciaro <jomgarciaro@unal.edu.co>
2022-01-23 08:06:48 -0500

    feat(layout): navigation changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,16 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+    <div class="max-w-7xl mx-auto px-2">
+        <div class="flex justify-between h-20">
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block mt-2 h-8 w-auto fill-current text-gray-600" />
+                        <img
+                            src="{{ asset('images/logo.png') }}" 
+                            alt="MercaTodo logo"
+                            width="150"
+                        />
                     </a>
                 </div>
 
