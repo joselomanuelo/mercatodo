1e5078393ca1f10ee0eb7625b57291d3974961bd
jomgarciaro <jomgarciaro@unal.edu.co>
2022-01-25 10:53:25 -0500

    style(admin/users): listeners

--- app/Listeners/AddLogoutToUsersLogsTable.php
+++ app/Listeners/AddLogoutToUsersLogsTable.php
@@ -4,15 +4,10 @@ namespace App\Listeners;
 
 use App\Actions\Users\AddInputUserLogs;
 use App\Events\UserLogouted;
-use App\Models\UserLog;
 use Illuminate\Support\Facades\Log;
 
 class AddLogoutToUsersLogsTable
 {
-    public function __construct()
-    {
-    }
-
     public function handle(UserLogouted $event): void
     {
         AddInputUserLogs::execute($event->user, 'logout');
