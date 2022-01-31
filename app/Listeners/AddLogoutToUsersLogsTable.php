4f29b9b89fef186f43d6c30c932826cab3fca201
Jomgarciaro <joselomanuelo@gmail.com>
2022-01-31 15:25:46 -0500

    refactor(admin/users): info indeed notice in logs

--- app/Listeners/AddLogoutToUsersLogsTable.php
+++ app/Listeners/AddLogoutToUsersLogsTable.php
@@ -12,6 +12,6 @@ class AddLogoutToUsersLogsTable
     {
         AddInputUserLogs::execute($event->user, 'logout');
 
-        Log::notice('The user with Id '.$event->user->id.' has logged out.');
+        Log::info('The user with Id '.$event->user->id.' has logged out.');
     }
 }
