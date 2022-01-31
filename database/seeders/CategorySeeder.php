488d7846e362a653aa1c17eee6379220a4c0be1f
Jomgarciaro <joselomanuelo@gmail.com>
2022-01-31 15:15:07 -0500

    feat(products): destroy categories cache

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,11 +5,14 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
+        Cache::forget('categories');
+
         Category::factory()->Has(Product::factory()->count(50))
             ->create([
                 'name' => trans('categories.cleaning'),
