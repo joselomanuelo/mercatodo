92c42a81b02514e42cadc8687db5d9b632243233
Jomgarciaro <joselomanuelo@gmail.com>
2022-02-08 13:04:38 -0500

    feat(products): API routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,12 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.Vue = require('vue');
+
+Vue.component('catalog', require('./components/Catalog.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    
+});
