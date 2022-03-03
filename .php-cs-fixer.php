2b4b88d6a145ee21791e174255c2d0092f4a8a89
jomgarciaro <jomgarciaro@unal.edu.co>
2022-03-03 18:50:01 -0500

    feat(ci): first github actions implementation

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,76 +4,63 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $rules = [
-    'array_syntax' => ['syntax' => 'short'],
+    '@PSR12' => true,
+    'phpdoc_indent' => true,
     'binary_operator_spaces' => [
-        'default' => 'single_space',
-        'operators' => ['=>' => null]
+        'operators' => ['=>' => null],
     ],
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
     'blank_line_before_statement' => [
-        'statements' => ['return']
+        'statements' => ['declare'],
     ],
     'braces' => true,
-    'cast_spaces' => true,
-    'class_attributes_separation' => [
-        'elements' => ['method' => 'one']
+    'cast_spaces' => [
+        'space' => 'none',
     ],
     'class_definition' => true,
     'concat_space' => [
-        'spacing' => 'one'
+        'spacing' => 'one',
     ],
     'declare_equal_normalize' => true,
     'elseif' => true,
     'encoding' => true,
     'full_opening_tag' => true,
-    'fully_qualified_strict_types' => true,
     'function_declaration' => true,
     'function_typehint_space' => true,
+    'single_line_comment_style' => [
+        'comment_types' => ['hash'],
+    ],
     'heredoc_to_nowdoc' => true,
     'include' => true,
-    'increment_style' => ['style' => 'post'],
     'indentation_type' => true,
     'linebreak_after_opening_tag' => true,
-    'line_ending' => true,
     'lowercase_cast' => true,
-    'constant_case' => true,
     'lowercase_keywords' => true,
-    'lowercase_static_reference' => true,
-    'magic_method_casing' => true,
+    'lowercase_static_reference' => true, // added from Symfony
     'magic_constant_casing' => true,
+    'magic_method_casing' => true, // added from Symfony
     'method_argument_space' => true,
+    'class_attributes_separation' => false,
+    'visibility_required' => true,
     'native_function_casing' => true,
     'no_alias_functions' => true,
-    'no_extra_blank_lines' => [
-        'tokens' => [
-            'extra',
-            'throw',
-            'use',
-            'use_trait',
-        ]
-    ],
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
     'no_closing_tag' => true,
     'no_empty_phpdoc' => true,
     'no_empty_statement' => true,
+    'no_extra_blank_lines' => true,
     'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
-    'no_mixed_echo_print' => [
-        'use' => 'echo'
-    ],
     'no_multiline_whitespace_around_double_arrow' => true,
-    'multiline_whitespace_before_semicolons' => [
-        'strategy' => 'no_multi_line'
-    ],
+    'multiline_whitespace_before_semicolons' => true,
     'no_short_bool_cast' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_after_function_name' => true,
     'no_spaces_around_offset' => true,
     'no_spaces_inside_parenthesis' => true,
     'no_trailing_comma_in_list_call' => true,
-    'no_trailing_comma_in_singleline_array' => true,
     'no_trailing_whitespace' => true,
     'no_trailing_whitespace_in_comment' => true,
     'no_unneeded_control_parentheses' => true,
@@ -85,32 +72,25 @@ $rules = [
     'not_operator_with_successor_space' => false,
     'object_operator_without_whitespace' => true,
     'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'phpdoc_indent' => true,
-    'general_phpdoc_tag_rename' => true,
-    'phpdoc_inline_tag_normalizer' => true,
-    'phpdoc_tag_type' => true,
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
     'phpdoc_scalar' => true,
     'phpdoc_single_line_var_spacing' => true,
     'phpdoc_summary' => true,
-    'phpdoc_to_comment' => true,
+    'phpdoc_to_comment' => false,
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
-    'phpdoc_var_without_name' => true,
-    'psr_autoloading' => true,
+    'phpdoc_var_without_name' => false,
+    'increment_style' => ['style' => 'post'],
+    'no_mixed_echo_print' => true,
     'self_accessor' => true,
-    'short_scalar_cast' => true,
+    'array_syntax' => ['syntax' => 'short'],
     'simplified_null_return' => false,
     'single_blank_line_at_eof' => true,
-    'single_blank_line_before_namespace' => true,
     'single_class_element_per_statement' => true,
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
-    'single_line_comment_style' => [
-        'comment_types' => ['hash']
-    ],
     'single_quote' => true,
     'space_after_semicolon' => true,
     'standardize_not_equals' => true,
@@ -120,30 +100,35 @@ $rules = [
     'trailing_comma_in_multiline' => true,
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
-    'visibility_required' => [
-        'elements' => ['method', 'property']
-    ],
+    'line_ending' => true,
     'whitespace_after_comma_in_array' => true,
+    'fully_qualified_strict_types' => true,
+
+    // customizations
     'no_unused_imports' => true,
+    'logical_operators' => true,
+    'short_scalar_cast' => true,
+    'no_unset_cast' => true,
+    'no_trailing_comma_in_singleline_array' => true,
+    'single_blank_line_before_namespace' => true,
 ];
 
-
 $finder = Finder::create()
-    ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
-    ])
+    ->notPath('bootstrap')
+    ->notPath('storage')
+    ->notPath('packages')
+    ->notPath('vendor')
+    ->in(getcwd())
     ->name('*.php')
     ->notName('*.blade.php')
+    ->notName('index.php')
+    ->notName('_ide_helper.php')
+    ->notName('server.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-$config = new Config();
-return $config->setFinder($finder)
+return (new Config())
+    ->setFinder($finder)
     ->setRules($rules)
     ->setRiskyAllowed(true)
     ->setUsingCache(true);
