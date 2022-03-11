0276ec9bef503e6db489b7104c82d40585954169
Jomgarciaro <joselomanuelo@gmail.com>
2022-03-11 10:06:37 -0500

    refactor(events): unused broadcasting

--- app/Events/OrderRejected.php
+++ app/Events/OrderRejected.php
@@ -3,9 +3,7 @@
 namespace App\Events;
 
 use App\Models\Order;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -20,10 +18,5 @@ class OrderRejected
     public function __construct(Order $order)
     {
         $this->order = $order;
-    }
-
-    public function broadcastOn(): Channel|array
-    {
-        return new PrivateChannel('channel-name');
     }
 }
